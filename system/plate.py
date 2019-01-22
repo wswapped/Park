29c0a75446c056c39f1bf016d38658e4bebc4138
wswapped <wswapped@gmail.com>
2019-01-22 22:15:11 +0530

    s

--- system/plate.py
+++ system/plate.py
@@ -34,40 +34,40 @@ def recognize(queue=None, frame=None):
 			if len(plateText) < 1:
 				# error occured
 				print("No text");
+			else:
+				# Parsing the plate text
+				plates = json.loads(plateText)
 
-			# Parsing the plate text
-			plates = json.loads(plateText)
+				returnPlates = []
 
-			returnPlates = []
+				cv2.imshow("Image in process", frameData)
 
-			cv2.imshow("Image in process", frameData)
-
-			pKey = cv2.waitKey(1)
-			if pKey & 0xFF == ord('x'):
-				break
-			elif pKey & 0xFF == ord('p'):
-				print("snapshot")
-				# Capture snapshot
-				snapFile = os.path.sep.join(('alpr', 'samples', "snapshot.jpg"))
-				cv2.imwrite(snapFile, frameData)
+				pKey = cv2.waitKey(1)
+				if pKey & 0xFF == ord('x'):
+					break
+				elif pKey & 0xFF == ord('p'):
+					print("snapshot")
+					# Capture snapshot
+					snapFile = os.path.sep.join(('alpr', 'samples', "snapshot.jpg"))
+					cv2.imwrite(snapFile, frameData)
 
 
-			# Looping through all plates
-			for plateFound in plates['results']:
-				returnPlates.append({'plate':plateFound['plate'], 'confidence':plateFound['confidence'], 'region':plateFound['coordinates']})
-			print("Candidates: "+str(returnPlates)+"\n")
-			detectedPlate = alpr.get_high_confidence(returnPlates)
-			print('True Plate: '+str(detectedPlate))
+				# Looping through all plates
+				for plateFound in plates['results']:
+					returnPlates.append({'plate':plateFound['plate'], 'confidence':plateFound['confidence'], 'region':plateFound['coordinates']})
+				print("Candidates: "+str(returnPlates)+"\n")
+				detectedPlate = alpr.get_high_confidence(returnPlates)
+				print('True Plate: '+str(detectedPlate))
 
-			if detectedPlate:
-				print("movement"+movementType)
-				serverInst = server('http://www.park.rw', 'api/index.php')
+				if detectedPlate:
+					print("movement"+movementType)
+					serverInst = server('http://www.park.rw', 'api/index.php')
 
-				if(movementType == 'entry'):
-					data = serverInst.enterCar(detectedPlate['plate'], 1, 1)
-				elif(movementType == 'exit'):
-					data = serverInst.exitCar(detectedPlate['plate'], 1, 1)
-				print(data)
+					if(movementType == 'entry'):
+						data = serverInst.enterCar(detectedPlate['plate'], 1, 1)
+					elif(movementType == 'exit'):
+						data = serverInst.exitCar(detectedPlate['plate'], 1, 1)
+					print(data)
 
 		else:
 			print("No frame found")
